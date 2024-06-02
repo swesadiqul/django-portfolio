8b576bb4330a435f1763831b27dc235574b87411
swesadiqul <mdsadiqulislam390@gmail.com>
2024-06-03 03:37:47 +0600

    vercel add

--- personal_portfolio_project/settings.py
+++ personal_portfolio_project/settings.py
@@ -122,5 +122,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
